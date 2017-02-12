935d751dbeb9f564e2099333907453c4278aa3d8
MarekVan <marek.kicnav@gmail.com>
2017-02-12 20:18:16 +0100

    linux HRT added

--- Winscard/stdafx.h
+++ Winscard/stdafx.h
@@ -247,8 +247,6 @@ typedef struct _WINSCARD_CONFIG {
 #ifndef RDTSC_H
 #define RDTSC_H
 
-#if defined(_WIN32)
-
 #if defined( _MSC_VER )
 #if defined( _WIN64 )
 #include <intrin.h>
@@ -257,18 +255,34 @@ __inline volatile unsigned long long read_tsc(void)
 {
     return __rdtsc();
 }
-#else
+#elif defined(_WIN32)
 __inline volatile unsigned long long read_tsc(void)
 {   unsigned long lo, hi;
     __asm cpuid __asm rdtsc __asm mov [lo],eax __asm mov [hi],edx ; 
     return (((unsigned long long)hi) << 32) + lo;
 }
-
 #endif
+
+#elif defined(__i386__)
+
+static __inline__ unsigned long long rdtsc(void)
+{
+	unsigned long long int x;
+	__asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
+	return x;
+}
+
+#elif defined(__x86_64__)
+
+static __inline__ unsigned long long rdtsc(void)
+{
+	unsigned hi, lo;
+	__asm__ __volatile__("rdtsc" : "=a"(lo), "=d"(hi));
+	return ((unsigned long long)lo) | (((unsigned long long)hi) << 32);
+}
+
 #else
 #error A high resolution timer is not available
-#endif
-
 #endif
 #endif
 
